b1d70f62453910e0d3858f68f5671ee45f853420
Tristan <tristanashephard@gmail.com>
2016-02-01 12:28:58 -0800

    Adding token authentication and custom html to login page

--- pushkin/settings.py
+++ pushkin/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'surveys',
 )
 
@@ -120,4 +121,10 @@ LOGGING = {
             'propagate': True,
         },
     },
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
