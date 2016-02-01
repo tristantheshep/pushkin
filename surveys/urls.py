11715e8761ea52dd904dfe7257e967aa1a157f7e
Tristan <tristanashephard@gmail.com>
2016-02-01 13:16:58 -0800

    Adding registration page and link

--- surveys/urls.py
+++ surveys/urls.py
@@ -2,6 +2,8 @@ from surveys import views
 
 from django.conf.urls import include
 from django.conf.urls import url
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
@@ -20,6 +22,9 @@ urlpatterns = [
     url(r'^surveys/(?P<sid>[0-9]+)/tags/$', views.TagList.as_view()),
     url(r'^surveys/(?P<sid>[0-9]+)/tags/(?P<pk>[0-9]+)$',
             views.TagDetail.as_view()),
+    url('^register/', CreateView.as_view(template_name='register.html',
+                                         form_class=UserCreationForm,
+                                         success_url='/'), name='register'),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
     url(r'^users/$', views.UserList.as_view()),
