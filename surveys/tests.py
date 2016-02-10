07510fed820a9ffafc69e8622b9f1ee453392a74
Tristan <tristanashephard@gmail.com>
2016-02-10 10:41:34 -0800

    Begin adding setup classes and decorators

--- surveys/tests.py
+++ surveys/tests.py
@@ -1,6 +1,5 @@
 
 from surveys.models import *
-from surveys.views import *
 
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
@@ -9,7 +8,24 @@ from rest_framework import status
 
 URLs = ['/surveys/','/surveys/1/','/surveys/1/questions/', '/surveys/1/questions/1', '/surveys/1/tags/', '/surveys/1/tags/1', '/surveys/1/responses/', '/surveys/1/responses/1', '/surveys/1/responses/1/answers/', '/surveys/1/responses/1/answers/1']
 
+class Setup:
+
+    @staticmethod
+    def oneUser(test_func):
+        def setup_func(suite):
+            # Setup
+            user = User.objects.create(username='user1')
+
+            # Execute the test
+            test_func(suite, user)
+
+            # Cleanup
+            user.delete()
+        return setup_func
+
+
 class AuthTests(APITestCase):
+
     def test_unauthenticated_get(self):
         """
         HTTP requests are met with 403s if not authenticated on all URLs
@@ -20,4 +36,6 @@ class AuthTests(APITestCase):
                 self.assertEqual(req(url).status_code, status.HTTP_403_FORBIDDEN,
                                  "Unauthenticated request %s/%s did not receive 403" % (req , url))
 
-        
+    @Setup.oneUser
+    def test_create_user(self, user):
+        print("Got user %s" % user)
