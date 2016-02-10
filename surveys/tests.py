62bef9d18b3359ddad04490ad596aba3e1ee3a07
Tristan <tristanashephard@gmail.com>
2016-02-09 17:21:43 -0800

    Test - adding unauthenticated get requests

--- surveys/tests.py
+++ surveys/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
 
-# Create your tests here.
+from surveys.models import *
+from surveys.views import *
+
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework.test import force_authenticate
+from rest_framework import status
+
+URLs = ['/surveys/','/surveys/1/','/surveys/1/questions/', '/surveys/1/questions/1', '/surveys/1/tags/', '/surveys/1/tags/1', '/surveys/1/responses/', '/surveys/1/responses/1', '/surveys/1/responses/1/answers/', '/surveys/1/responses/1/answers/1']
+
+class AuthTests(APITestCase):
+    def test_unauthenticated_get(self):
+        for url in URLs:
+            self.assertEqual(self.client.get(url).status_code, status.HTTP_403_FORBIDDEN,
+                             "Unauthenticated get request did not receive 403 for url %s" % url)
+
