220b5bbda466a5d2d5986440710b33f368002702
Tristan <tristanashephard@gmail.com>
2016-02-10 14:08:28 -0800

    DRY up the AuthTests a lot!

--- surveys/tests.py
+++ surveys/tests.py
@@ -12,22 +12,30 @@ URLs = ['/surveys/','/surveys/1/','/surveys/1/questions/', '/surveys/1/questions
 class TestBase(APITestCase):
 
     def setUp(self):
-        user0 = User.objects.create(username='user0')
-        user1 = User.objects.create(username='user1')
-        user2 = User.objects.create(username='user2')
 
-        # Add some surveys to user0
-        user0.surveys.create(name='survey0_1')
-        user0.surveys.create(name='survey0_2')
-        user0.surveys.create(name='survey0_3')
+        # Create a couple of users
+        for i in range(2):
+            user = User.objects.create(username='user_' + str(i))
+            # Add a couple of surveys for each user
+            for j in range(2):
+                survey = user.surveys.create(name=user.username + '_survey_' + str(j))
+                # Add a couple of tags, questions, responses to each survey
+                for k in range(2):
+                    survey.tag_options.create(tag_text=survey.name + "_tag_opt_" + str(k))
+                    survey.questions.create(question_text=survey.name + "_question_" + str(k))
+                    survey.responses.create()
 
-        # Add some surveys to user1
-        user1.surveys.create(name='survey1_1')
-        user1.surveys.create(name='survey1_2')
-        user1.surveys.create(name='survey1_3')
+        # Authenticate requests from the first user by default
+        self.client.force_authenticate(user = self.users[0])
 
-        # Authenticate requests from user0 by default
-        self.client.force_authenticate(user=user0)
+        # Generate a list of all URIs for two of the users
+        user1, user2, *_ = self.users
+        self.user1_uris = self._all_user_uris(user1)
+        self.user2_uris = self._all_user_uris(user2)
+
+        # Keep a list of requests handy
+        self.requests = [self.client.get, self.client.post, self.client.put, 
+                         self.client.patch, self.client.delete]
 
     def tearDown(self):
         self.users.delete()
@@ -36,21 +44,62 @@ class TestBase(APITestCase):
     def users(self):
         return User.objects.all()
 
+    def check_response_code(self, uri, method, exp_response_code):
+        """
+        Assert we get a certain response code given a URI and http method
+        """
+        response_code = method(uri).status_code
+        self.assertEqual(response_code, exp_response_code,
+                         "Expected %s for request '%s %s', got %s"
+                         % (exp_response_code, method.__name__,
+                            uri, response_code))
+
+    def _all_user_uris(self, user):
+        """
+        Generate a list of all URIs for a given user, e.g. all surveys,
+        all questions for each survey, etc.
+        """
+        uris = []
+        for survey in user.surveys.all():
+            survey_uri = '/surveys/%s/' % survey.id
+            questions_uri = survey_uri + 'questions/'
+            tags_uri = survey_uri + 'tags/'
+            responses_uri = survey_uri + 'responses/'
+
+            uris.append(survey_uri)
+            uris.append(questions_uri)
+            uris.append(tags_uri)
+            uris.append(responses_uri)
+
+            for ix in range(1, survey.questions.count() + 1):
+                uris.append(questions_uri + '%s/' % ix)
+
+            for ix in range(1, survey.tag_options.count() + 1):
+                uris.append(tags_uri + '%s/' % ix)
+
+            for ix, response in enumerate(survey.responses.all()):
+                response_uri = responses_uri + '%s/' % (ix+1)
+                answers_uri = response_uri + 'answers/'
+                uris.append(response_uri)
+                uris.append(answers_uri)
+
+                for jx in range(1, response.answers.count() + 1):
+                    uris.append(answers_uri + '%s/' % jx)
+                
+        return uris
 
 class AuthTests(TestBase):
 
     def test_unauthenticated_requests(self):
         """
-        HTTP requests are met with 403s if not authenticated on all URLs
+        All HTTP requests on all URIs are met with 403s if not authenticated
         """
         # Remove authentication from client
         self.client.force_authenticate()
 
-        requests = [self.client.get, self.client.post, self.client.put, self.client.patch, self.client.delete]
-        for req in requests:
-            for url in URLs:
-                self.assertEqual(req(url).status_code, status.HTTP_403_FORBIDDEN,
-                                 "Unauthenticated request %s/%s did not receive 403" % (req , url))
+        for uri in self.user1_uris + self.user2_uris + ['/surveys/']:
+            for req in self.requests:
+                self.check_response_code(uri, req, status.HTTP_403_FORBIDDEN)
 
     def test_auth_token(self):
         """
@@ -60,25 +109,20 @@ class AuthTests(TestBase):
         keys = set(u.auth_token.key for u in users)
         self.assertEqual(len(keys), len(users), "Some users have the same key")
 
-    def test_get_others_survey(self):
+    def test_own_survey_access(self):
         """
-        User is met with 403 when trying to access another user's survey
+        User gets 200 when accessing their own URIs
         """
-        # Make sure that we *can* get a survey belonging to user0
-        survey_id = self.users[0].surveys.first().id
-        uri = '/surveys/%s/' % survey_id
-        response = self.client.get(uri).status_code
-        self.assertEqual(response, status.HTTP_200_OK,
-                         "Expected 200 for GET request on %s, got %s" %
-                                                                (uri, response))
+        for uri in self.user1_uris:
+            self.check_response_code(uri, self.client.get, status.HTTP_200_OK)
 
-        # Try and get a survey from user1
-        survey_id = self.users[1].surveys.first().id
-        uri = '/surveys/%s/' % survey_id
-        response = self.client.get(uri).status_code
-        self.assertEqual(response, status.HTTP_403_FORBIDDEN,
-                         "Expected 403 for GET request on %s, got %s" % 
-                                                                (uri, response))        
+    def test_others_survey_access(self):
+        """
+        User is met with 403 when trying to access another user's URIs
+        """
+        for uri in self.user2_uris:
+            self.check_response_code(uri, self.client.get, 
+                                     status.HTTP_403_FORBIDDEN)
 
-
+         
 
